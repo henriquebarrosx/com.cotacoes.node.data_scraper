d496d9f062a00c9260f32d57a95afcf29bbb6756
Henrique Barros <henriquebarrosx@gmail.com>
2025-09-05 21:06:36 -0300

    adiciona tratamento a data do TheNews para evitar problemas de timezone

--- src/app/worker/the_news/the_news_url.ts
+++ src/app/worker/the_news/the_news_url.ts
@@ -21,7 +21,11 @@ export class TheNewsUrl {
 	}
 
 	#getDateFormat() {
-		const date = new Date(this.#isoDate);
+		const zonedDate = this.#isoDate
+			.split('T')
+			.join('T12:00:00');
+
+		const date = new Date(zonedDate);
 
 		const day = String(date.getDate()).padStart(2, '0');
 		const month = String(date.getMonth() + 1).padStart(2, '0');
