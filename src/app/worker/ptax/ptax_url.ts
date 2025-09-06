04bc3227b15a248453915cc3ff4eb8bf22e95a12
Henrique Barros <henriquebarrosx@gmail.com>
2025-09-05 21:16:54 -0300

    corrige formatação de data do TheNews e Ptax

--- src/app/worker/ptax/ptax_url.ts
+++ src/app/worker/ptax/ptax_url.ts
@@ -23,16 +23,14 @@ export class PtaxUrl {
 	}
 
 	#getDateFormat() {
-		const zonedDate = this.#isoDate
+		const dateFormat = this.#isoDate
 			.split('T')
-			.join('T12:00:00');
+			.slice(0, 1)
+			.join('')
+			.split('-')
+			.reverse()
+			.join('/')
 
-		const date = new Date(zonedDate);
-
-		const day = String(date.getDate()).padStart(2, '0');
-		const month = String(date.getMonth() + 1).padStart(2, '0');
-		const year = date.getFullYear();
-
-		return `${day}/${month}/${year}`;
+		return dateFormat;
 	}
 }
--- src/app/worker/the_news/the_news_url.ts
+++ src/app/worker/the_news/the_news_url.ts
@@ -21,16 +21,14 @@ export class TheNewsUrl {
 	}
 
 	#getDateFormat() {
-		const zonedDate = this.#isoDate
+		const dateFormat = this.#isoDate
 			.split('T')
-			.join('T12:00:00');
+			.slice(0, 1)
+			.join('')
+			.split('-')
+			.reverse()
+			.join('/')
 
-		const date = new Date(zonedDate);
-
-		const day = String(date.getDate()).padStart(2, '0');
-		const month = String(date.getMonth() + 1).padStart(2, '0');
-		const year = date.getFullYear();
-
-		return `${day}/${month}/${year}`;
+		return dateFormat;
 	}
 }
