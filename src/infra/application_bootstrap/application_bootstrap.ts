6b5222c136ed1d7e3d7f8d37c93273247ba5bfdd
Henrique Barros <henriquebarrosx@gmail.com>
2025-10-07 10:51:32 -0300

    fix: remove busca do cme hardcoded

--- src/infra/application_bootstrap/application_bootstrap.ts
+++ src/infra/application_bootstrap/application_bootstrap.ts
@@ -4,7 +4,6 @@ import { browserManager } from '../browser_manager/index.ts';
 import { cmeQueueConsumer } from "../../app/queue/cme_queue_consumer/index.ts";
 import { ptaxQueueConsumer } from "../../app/queue/ptax_queue_consumer/index.ts";
 import { theNewsQueueConsumer } from "../../app/queue/the_news_queue_consumer/index.ts";
-import { queues } from "../message_broker/queues.ts";
 
 export class ApplicationBootstrap {
 
@@ -20,13 +19,6 @@ export class ApplicationBootstrap {
 			await ptaxQueueConsumer.register();
 			await cmeQueueConsumer.register();
 			await theNewsQueueConsumer.register();
-
-			await messageBroker.publish(
-				{
-					to: queues.CME_DATA_SCRAPER,
-					message: null,
-				}
-			)
 		}
 
 		catch {
