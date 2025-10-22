4825777eb094096db6b43f5f90f78341e96c53b9
Henrique Barros <henriquebarrosx@gmail.com>
2025-10-22 11:45:55 -0300

    chore: adiciona fallback para reiniciar a aplicação quando algum erro interromper sua execução

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,7 @@
 import { applicationBoostrap } from "./infra/application_bootstrap/index.ts";
 
 await applicationBoostrap.init();
+
+process.once('SIGHUP', async () => {
+    await applicationBoostrap.init();
+});
